9d9131e56f3e7ef021f11457276ea97d086be903
friendly-flame <armsnyder@gmail.com>
2015-04-07 11:28:29 -0500

    Added documentation to developer scripts

--- developer_scripts/parse.py
+++ developer_scripts/parse.py
@@ -1,3 +1,19 @@
+# This is a file of python functions that interact with our Parse database.
+# They're not a part of the app. They're for us to use to populate the database.
+
+# Adding recipes:
+#     Run >> python parse.py add_recipe
+#     Type recipes in the format recipe_name = ingredient1, ingredient2, etc.
+#     The script handles recognizing existing ingredients and adding new ones.
+#
+# Maintaining recipes:
+#     1. Edit the file recipes.txt
+#     2. Purge our Parse database
+#     3. Run >> python parse.py load_recipes
+#     The script loads each line of recipes.txt into the database.
+
+
+
 import sys
 import httplib
 import ast
@@ -12,6 +28,9 @@ headers = {
 
 
 def add_object(table, obj):
+    """
+    Adds object (must be a JSON string) to a given table
+    """
     conn = httplib.HTTPSConnection('api.parse.com')
     conn.request("POST", "/1/classes/"+table, obj, headers)
     response = conn.getresponse()
@@ -23,6 +42,9 @@ def add_object(table, obj):
 
 
 def request_table(table):
+    """
+    Returns a database table as a dictionary object
+    """
     conn = httplib.HTTPSConnection('api.parse.com')
     conn.request("GET", "/1/classes/"+table, '', headers)
     response = conn.getresponse()
@@ -38,11 +60,17 @@ def show_usage():
 
 
 def add_ingredient(ingredient_name):
+    """
+    Adds an ingredient to the ingredient database
+    """
     ingredient_object = '{"name": "'+ingredient_name+'"}'
     return add_object('Ingredients', ingredient_object)
 
 
 def add_recipe_prompt():
+    """
+    Shows a looping user prompt for adding recipes to the database
+    """
     recipe_raw = raw_input('Enter recipe: ')
     if not recipe_raw:
         return
@@ -51,6 +79,9 @@ def add_recipe_prompt():
 
 
 def add_recipe(recipe_raw):
+    """
+    Adds a recipe to the recipe database
+    """
     tokenized_recipe = recipe_raw.split('=')
     recipe_name = tokenized_recipe[0].strip()
     recipe_ingredients = [ingredient.strip() for ingredient in tokenized_recipe[1].split(',')]
@@ -71,11 +102,15 @@ def add_recipe(recipe_raw):
 
 
 def load_recipes():
+    """
+    Loads recipes from text file
+    """
     with open('recipes.txt') as recipe_file:
         for line in recipe_file.readlines():
             add_recipe(line)
 
 
+# Main function (checks sys args and executes above functions
 if __name__ == '__main__':
     args = sys.argv[1:]
     if not args:
