969bc658ffc80f9ca39d4344974dfcd59d7778f4
missfxhr <missfxhr@gmail.com>
2015-04-08 18:40:42 -0500

    minor changes

--- CloudCode/testController.js
+++ CloudCode/testController.js
@@ -1,3 +1,4 @@
+Parse.initialze("Et6HrDXxBYdz4eQRUTnqH6HtTOTWwW9chrKXRYTe","gIPArJcAQFVGCoVLKuJoIRGGzoG9gL5IDCq1NWPI");
 Parse.Cloud.run('search4Recipes', { ingredientNames: ["coffee","whipped cream"] }, {
 	success: function(recipeList) {
     	document.getElementById("test").innerHTML = recipeList[0].get("name");
