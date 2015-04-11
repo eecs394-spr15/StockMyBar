a1e5fce84ec87a2079ba60a2506db5bddb7db436
friendly-flame <armsnyder@gmail.com>
2015-04-11 18:57:18 -0500

    Items list is sorted alphabetically. I had to change the ingredient list format a bit to make it work. Closes #28.

--- app/common/services/MyBarService.js
+++ app/common/services/MyBarService.js
@@ -12,7 +12,7 @@ angular
             success: function(results) {
                 factory.allIngredients = [];
                 for(var i=0; i<results.length; i++) {
-                    factory.allIngredients.push(results[i].get("name"));
+                    factory.allIngredients.push({name: results[i].get("name")});
                 }
                 }
             });
--- app/inputs/controllers/inputCtrl.js
+++ app/inputs/controllers/inputCtrl.js
@@ -3,21 +3,21 @@
 angular
 	.module('inputs')
     .controller('InputCtrl', function ($scope, MyBarService, supersonic) {
-        $scope.checkedIngredients = [];
+        $scope.checkedIngredients = {};
         $scope.allIngredients = [];
         $scope.updateService = function() {
             // Updates user's bar contents whenever they make a new selection in the Items view
             $scope.recipes = [];
             temp_barContents = [];
-            for (var i = 0; i < $scope.checkedIngredients.length; i++) {
-                if ($scope.checkedIngredients[i].checked) {
-                    temp_barContents.push($scope.checkedIngredients[i].item);
+            angular.forEach($scope.checkedIngredients, function(value, key) {
+                if(value) {
+                    temp_barContents.push(key);
                 }
-            }
+            });
             supersonic.data.channel('barContents').publish(temp_barContents);
         };
         $scope.clearAllItems = function() {
-            $scope.checkedIngredients = [];
+            $scope.checkedIngredients = {};
             reset();
             $scope.updateService();
         };
@@ -25,9 +25,11 @@ angular
         var reset = function() {
             supersonic.logger.log("Resetting ingredients");
             $scope.allIngredients = MyBarService.allIngredients;
+            supersonic.logger.log($scope.allIngredients);
             angular.forEach($scope.allIngredients,function(item){
-                $scope.checkedIngredients.push({item:item,checked:false});
+                $scope.checkedIngredients[item.name] = false;
             });
+            supersonic.logger.log($scope.checkedIngredients);
             $scope.$apply();
         }
     });
