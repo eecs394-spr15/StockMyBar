b3b415d98e092a7ed8c95efc2b976bd379fc4ea0
missfxhr <missfxhr@gmail.com>
2015-04-13 16:47:11 -0500

    fix merge problem

--- app/inputs/controllers/inputCtrl.js
+++ app/inputs/controllers/inputCtrl.js
@@ -3,17 +3,12 @@
 angular
 	.module('inputs')
     .controller('InputCtrl', function ($scope, MyBarService, supersonic) {
-<<<<<<< HEAD
         $scope.checkedIngredients = {};
-=======
-        $scope.checkedIngredients = [];
->>>>>>> feature/database-algorithm
         $scope.allIngredients = [];
         $scope.updateService = function() {
             // Updates user's bar contents whenever they make a new selection in the Items view
             $scope.recipes = [];
             temp_barContents = [];
-<<<<<<< HEAD
             angular.forEach($scope.checkedIngredients, function(value, key) {
                 if(value) {
                     temp_barContents.push(key);
@@ -23,17 +18,6 @@ angular
         };
         $scope.clearAllItems = function() {
             $scope.checkedIngredients = {};
-=======
-            for (var i = 0; i < $scope.checkedIngredients.length; i++) {
-                if ($scope.checkedIngredients[i].checked) {
-                    temp_barContents.push($scope.checkedIngredients[i].item);
-                }
-            }
-            supersonic.data.channel('barContents').publish(temp_barContents);
-        };
-        $scope.clearAllItems = function() {
-            $scope.checkedIngredients = [];
->>>>>>> feature/database-algorithm
             reset();
             $scope.updateService();
         };
@@ -41,7 +25,6 @@ angular
         var reset = function() {
             supersonic.logger.log("Resetting ingredients");
             $scope.allIngredients = MyBarService.allIngredients;
-<<<<<<< HEAD
             supersonic.logger.log($scope.allIngredients);
             angular.forEach($scope.allIngredients,function(item){
                 $scope.checkedIngredients[item.name] = false;
@@ -52,11 +35,4 @@ angular
         $scope.clearSearchBarText = function() {
             $scope.searchBarText = '';
         };
-=======
-            angular.forEach($scope.allIngredients,function(item){
-                $scope.checkedIngredients.push({item:item,checked:false});
-            });
-            $scope.$apply();
-        }
->>>>>>> feature/database-algorithm
     });
--- app/results/controllers/resultsCtrl.js
+++ app/results/controllers/resultsCtrl.js
@@ -5,20 +5,12 @@ angular
 	.controller('ResultsCtrl', function ($scope, supersonic, MyBarService) {
 		var ingList = [];
 		$scope.recipes = [];
-<<<<<<< HEAD
-
-=======
 		$scope.noneActive = true;
->>>>>>> feature/database-algorithm
 		supersonic.data.channel('barContents').subscribe( function(newVal) {
 			// Updates possible recipes anytime the user's bar contents change
 			ingList = newVal;
 			Parse.initialize("Et6HrDXxBYdz4eQRUTnqH6HtTOTWwW9chrKXRYTe", "gIPArJcAQFVGCoVLKuJoIRGGzoG9gL5IDCq1NWPI");
-<<<<<<< HEAD
-			Parse.Cloud.run("search4Recipes", {ingredientNames: ingList}, {
-=======
 			Parse.Cloud.run("search4Recipes2", {ingredientNames: ingList}, {
->>>>>>> feature/database-algorithm
 				success: function(results) {
 					$scope.recipes = results;
 					$scope.noneActive = true;
@@ -31,56 +23,12 @@ angular
 		});
 
 		// Change activeRecipe on UI click
-<<<<<<< HEAD
-		$scope.noneActive = true;
 		$scope.activateRecipe = function(index) {
-			$scope.noneActive = false;
-			$scope.selected = index;
-			$scope.activeRecipe =  $scope.recipes[index];
-=======
-			$scope.activateRecipe = function(index) {
 			$scope.noneActive = false;
 			$scope.selected = index;
 			$scope.activeRecipe =  $scope.recipes[index];
 			$scope.neededIngredients = $scope.recipes[index].ingredListOffHand;
 			$scope.haveIngredients = $scope.recipes[index].ingredListInHand;
 			$scope.$apply();
-			/*
->>>>>>> feature/database-algorithm
-			recipeId = $scope.activeRecipe.get('objectId');
-			var query = new Parse.Query("Join_Table");
-			query.include("ingredient");
-			query.equalTo("recipe", $scope.activeRecipe);
-			query.find({
-  				success: function(results) {
-					ingredientList = [];
-					haveIngredientList = [];
-  					for (var i = 0; i < results.length; i++) {
-						currentName = results[i].get("ingredient").get("name");
-						if (ingList.indexOf(currentName) == -1) {
-							ingredientList.push(currentName);
-						} else {
-							haveIngredientList.push(currentName);
-						}
-  					}
-
-					$scope.neededIngredients = ingredientList;
-					$scope.haveIngredients = haveIngredientList;
-<<<<<<< HEAD
-					$scope.$apply()
-=======
-					$scope.$apply();
->>>>>>> feature/database-algorithm
-  				},
-				error: function(error) {
-					$scope.test = 'baz';
-				}
-  			});
-<<<<<<< HEAD
-=======
-			*/
->>>>>>> feature/database-algorithm
 		};
-
-
 	});
