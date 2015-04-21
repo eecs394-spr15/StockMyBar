121be5c8e4b99f71fe308e78173ef8f4401250e9
Frank Avino <francisavino2015@u.northwestern.edu>
2015-04-21 12:18:36 -0500

    Added view of preferences, but need to write logic to modify results based on user's preferences

--- app/inputs/services/MyBarService.js
+++ app/inputs/services/MyBarService.js
@@ -5,6 +5,7 @@ angular
 	.factory('MyBarService', function(supersonic) {
         factory = {};
         factory.allIngredients = [];
+        factory.allPreferences = [];
 
         supersonic.logger.log("Getting all ingredients from database");
         Parse.initialize("Et6HrDXxBYdz4eQRUTnqH6HtTOTWwW9chrKXRYTe", "gIPArJcAQFVGCoVLKuJoIRGGzoG9gL5IDCq1NWPI");
@@ -16,6 +17,19 @@ angular
                     factory.allIngredients.push({name: results[i].get("name")});
                 }
                 supersonic.data.channel('allIngredients').publish(factory.allIngredients);
+            }, error: function(error) {
+                supersonic.logger.log(error);
+            }
+        });
+
+        var query = new Parse.Query("Preferences");
+        query.find({
+            success: function (results) {
+                factory.allPreferences = [];
+                for (var i = 0; i < results.length; i++) {
+                    factory.allPreferences.push({name: results[i].get("name")});
+                }
+                supersonic.data.channel('allPreferences').publish(factory.allPreferences);
             }, error: function(error) {
                 supersonic.logger.log(error);
             }
