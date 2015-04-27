9c976cb9aa44362c8b7959381f3c8d47a2003670
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2015-04-27 00:04:47 -0500

    ingredients view done

--- app/inputs/controllers/categoriesCtrl.js
+++ app/inputs/controllers/categoriesCtrl.js
@@ -5,6 +5,11 @@ angular
     .controller('CategoriesCtrl', function ($scope, MyBarService, supersonic) {
 
 
-
+    	/* On category click --> show items to select within category */ 
+    	$scope.chooseCategory = function(category){
+            localStorage.category = category;
+            var view = new supersonic.ui.View("inputs#itemSelect");
+			supersonic.ui.layers.push(view);
+        }
 
     });
