1c0516358d0b967ea2c2b533ec2f320fb82c47f3
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2015-04-28 00:49:13 -0500

    readded code into itemsCtrl

--- app/inputs/controllers/itemsCtrl.js
+++ app/inputs/controllers/itemsCtrl.js
@@ -5,11 +5,43 @@ angular
     .controller('ItemsCtrl', function ($scope, supersonic) {
 
 
+        /*********************
+         *                   *
+         *  Data management  *
+         *                   *
+         *********************/
+
+        // Initializations
+        $scope.ingredIdList = angular.isDefined(localStorage.ingredIdList) ? JSON.parse(localStorage.ingredIdList) : [];
+        $scope.ingredList = [];
+        $scope.showActions = false;
+
+        // Publish ingredient list to the appropriate channel
+        setTimeout(function() {
+            supersonic.data.channel('ingredIdList').publish($scope.ingredIdList);
+        }, 1000);
+
+        // Update ingredient list
+        updateIngredList();
+
+        // Update ingredient list
+        function updateIngredList(){
+            $scope.ingredList = angular.isDefined(localStorage.ingredList) ? JSON.parse(localStorage.ingredList) : [];
+            var newList = [];
+            for(var i=0;i<$scope.ingredList.length;i++){
+                newList.push(createIngredPartJS($scope.ingredList[i].id,$scope.ingredList[i].name));
+            }
+            $scope.ingredList = newList;
+            $scope.$apply();
+        }
+
 
        
-        /*
-         * UI Interaction Functionality
-         */
+        /**********************************
+         *                                *
+         *  UI Interaction Functionality  *
+         *                                *
+         **********************************/
 
         // Clear all ingredients from user's bar
         $scope.clearAllItems = function() {
@@ -20,18 +52,25 @@ angular
             supersonic.data.channel('ingredIdList').publish($scope.ingredIdList);
             $scope.apply();
         };
+
         // Navigate to categories view to add a user's ingredient
         $scope.addIngredient = function() {
             var view = new supersonic.ui.View("inputs#categories");
             supersonic.ui.layers.push(view);
         };
 
+        // Select ingredient
+        $scope.selectIngredient = function() {
+
+        }
 
 
 
-        /*
-         * Styling
-         */
+        /*************
+         *           *
+         *  Styling  *
+         *           *
+         *************/
 
         // Inject shelves
         setTimeout(function() {
